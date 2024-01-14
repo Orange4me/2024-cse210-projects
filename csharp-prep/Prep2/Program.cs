20f270f06b2ed07c3938dde116cb6e5c5baf0974
Brandon Alder <97628281+Orange4me@users.noreply.github.com>
2024-01-13 22:49:25 -0700

    Week 1 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string score = Console.ReadLine();
+        float percent = float.Parse(score);
+        
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = ("A");
+        }
+        else (percent >= 80)
+        {
+            letter = ("B");
+        }
+        else (percent >= 70)
+        {
+            letter = ("C");
+        }
+        else (percent >= 60)
+        {
+            letter = ("D");
+        }
+        else 
+        {
+            letter = ("F");
+        }
+
+        Console.WriteLine($"Grade: {letter}")
+
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats! You passed.");
+        }
+        else 
+        {
+            Console.WriteLine("Give it another shot.");
+        }
     }
 }
