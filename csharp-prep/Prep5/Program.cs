f4edc6e887ffc9963ee5e5be27340e6862a89ca4
Brandon Alder <97628281+Orange4me@users.noreply.github.com>
2024-01-17 23:52:45 -0700

    week2prep

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string GetName()
+        {
+            Console.WriteLine("What is your name? ")
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int GetNumber()
+        {
+            Console.WriteLine("How old are you? ")
+            string numb = Console.ReadLine();
+            int number = int.Parse(numb);
+            return number;
+        }
+
+        static int MultiplyNumber(int number)
+        {
+            int squared = number * number;
+            return squared;
+        }
+
+        static void IterateData(string name, int squared)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squared}.")
+        }
     }
 }
